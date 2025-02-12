bc5412fa27657921e3d7c0519cab8e886f615483
Diego Lauar <diego_pktd@hotmail.com>
2025-02-12 17:02:57 -0300

    Primeiro use-case & test

--- src/domain/entities/student.ts
+++ src/domain/entities/student.ts
@@ -1,12 +1,13 @@
 import { randomUUID } from "node:crypto"
 
-export class Instructor {
+class Student {
     public id: string
     public name: string
 
-    constructor(name: string, id?: string){
+    constructor(name: string, id?: string) {
         this.name = name
         this.id = id ?? randomUUID()
-
+        
     }
 }
+
