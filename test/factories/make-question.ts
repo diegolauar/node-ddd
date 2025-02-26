90e90b5f74320e5c86bc9dcebf1a988b454e658c
Diego Lauar <diego_pktd@hotmail.com>
2025-02-26 10:19:52 -0300

    Deletar pergunta

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,14 +1,21 @@
 import { UniqueEntityID } from "../../src/core/entities/unique-entity-id"
 import { Question, QuestionProps } from "../../src/domain/forum/enterprise/entities/question"
+import { faker } from '@faker-js/faker'
 
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-    const question = Question.create({
-        authorId: new UniqueEntityID(),
-        title: 'Example',
-        content: 'Example content',
-        ...override,
-    })
+export function makeQuestion(
+    override: Partial<QuestionProps> = {},
+    id?: UniqueEntityID,
+) {
+    const question = Question.create(
+        {
+            authorId: new UniqueEntityID(),
+            title: faker.lorem.sentence(),
+            content: faker.lorem.text(),
+            ...override,
+        },
+        id,
+    )
 
     return question
 }
